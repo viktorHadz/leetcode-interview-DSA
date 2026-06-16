db923a4a794dc563b9f30f53bfffee8835ad9f92
Viktor Hadzhiyski <vikecah@gmail.com>
2026-06-16 01:00:41 +0100

    sliding window

--- arrAndStr/slidingWindow.js
+++ arrAndStr/slidingWindow.js
@@ -52,3 +52,23 @@ function lgSum(nums, k) {
 }
 
 console.log(lgSum(arr, 4));
+
+// ---------------------------------------------------------------------------------------
+// Given an integer array nums consisting of n elements and an integer k.
+// Find a contiguous subarray whose length is equal to k.
+// That has the maximum average value and return this value
+// Any answer with a calculation error less than 10^-5 will be accepted
+//
+// example 1
+// Input: nums = [1,12,-5,-6,50,3], k = 4
+// Output: 12.75000
+// Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+
+function maxAvgSubArr(nums, k) {
+  let curr = 0;
+  // build initial window. Current should be 2
+  for (let i = 0; i < k; i++) {
+    curr += nums[i];
+  }
+  console.log("Current: ", curr);
+}
