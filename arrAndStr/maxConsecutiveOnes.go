787a7401fe5f1a48cdd265221cbf279e63d1e17e
Viktor Hadzhiyski <vikecah@gmail.com>
2026-07-04 08:38:54 +0100

    more dynamic window

--- arrAndStr/maxConsecutiveOnes.go
+++ arrAndStr/maxConsecutiveOnes.go
@@ -1,70 +1,108 @@
 package arrandstr
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-// Sliding window - dynamic
-func MaxOnes3(nums []int, k int) int {
-	left := 0
-	flipCount := k
-	maxScore := 1
+func MaxAvg(nums []int, k int) float64 {
+	sum := 0
+	for i := range k {
+		sum += nums[i]
+	}
+	maxSum := sum
 
-	score := 1
-
-	for right := range nums {
-		fmt.Println("left:", left, "right:", right, "score:", score)
-		fmt.Println("flipCount:", flipCount)
-		fmt.Println("nums[left]:", nums[left], "nums[right]", nums[right])
-		fmt.Println("------------------------------")
-		if flipCount == 0 {
-			left++
-			flipCount++
-			score = 0
-		}
-
-		if nums[right] == 0 {
-			flipCount--
-		}
-
-		score++
-		if score > maxScore {
-			maxScore = score
+	for i := k; i < len(nums); i++ {
+		sum := nums[i-k] + nums[i]
+		fmt.Println("Sum: ", sum)
+		if sum > maxSum {
+			maxSum = sum
 		}
 	}
-	fmt.Println("maxScore:", maxScore)
-	return maxScore
 
+	fmt.Println("maxSum", float64(maxSum)/float64(k))
+	return float64(maxSum) / float64(k)
+}
+
+// Sliding window - dynamic
+func MinSubArrSum(nums []int, target int) float64 {
+	min := math.Inf(+1)
+	start := 0
+	end := 0
+	sum := 0
+	for end < len(nums) {
+		sum += nums[end]
+		end++
+
+		for start < end && sum >= target {
+			sum -= nums[start]
+			start++
+			min = math.Min(min, float64(end-start+1))
+			fmt.Println("Start:", start, "End:", end, "Min:", min)
+		}
+	}
+
+	return min
 }
 
 func MaxOnes(nums []int, k int) int {
+	max := 0
 	left := 0
-	right := 0
+	sum := 0
+	zerosInWindow := 0
 
-	maxScore := 0
-	flipCount := k
-	for i := 0; i < len(nums)-1; i++ {
-		curr := 0
-		for flipCount > 0 && right < len(nums)-1 {
-			if nums[i] == 0 {
-				flipCount--
-			}
-			right++
-			curr++
-			fmt.Println("Current Score =", curr)
+	for right := 0; right < len(nums); right++ {
+		// Add nums[right] to window (expand)
+		if nums[right] == 0 {
+			zerosInWindow++
 		}
-
-		fmt.Println("Left: ", left)
-		fmt.Println("Right: ", right)
-
-		left++
-		flipCount = k
-
-		if curr > maxScore {
-			maxScore = curr
+		sum++
+		fmt.Println("Zeroes: ", zerosInWindow, "Left: ", left, "Right: ", right, "Max: ", max)
+		// While window condition is violated (shrink)
+		for zerosInWindow > k {
+			if nums[left] == 0 {
+				zerosInWindow--
+			}
+			left++
+			// Current window size
+			currentSize := right - left + 1
+			fmt.Println("--- Shrinking --- ", "Current win size: ", currentSize)
+			if currentSize > max {
+				max = currentSize
+			}
 		}
 	}
-
-	fmt.Println("Final maxScore = ", maxScore)
-	return maxScore
+	return int(max)
 }
 
-// again man i hate being dumb
+func LongestWithSumLessThan(nums []int, k int) int {
+	start := 0
+	subLen := 0
+	longest := 0
+	sum := 0
+
+	for end := 0; end < len(nums); end++ {
+		sum += nums[end]
+		subLen++
+		fmt.Println("++++++++++\n", "Subarray Length: ", subLen, "Longest: ", longest, "Sum: ", sum)
+		fmt.Println("Start: ", start, "End: ", end)
+		fmt.Println("++++++++++")
+
+		if sum <= k && subLen > longest {
+			longest = subLen
+			fmt.Println("    Updating longest: ", longest)
+		}
+
+		for sum > k {
+			sum -= nums[start]
+			subLen--
+			start++
+			fmt.Println("-----------\n Sum > k: ", "Sum: ", sum, "Subarray Length: ", subLen, "Longest: ", longest)
+			fmt.Println("Start: ", start, "End: ", end)
+			fmt.Println("-----------")
+		}
+
+	}
+	fmt.Println("LONGEST TO RETURN: ", longest)
+	return longest
+}
--- main.go
+++ main.go
@@ -19,9 +19,11 @@ func main() {
 
 	// Sliding Win - Static
 	// arrAndStr.MaxAvgSubArr([]int{1, 12, -5, -6, 50, 3}, 4)
-
+	// arrAndStr.MaxAvg([]int{1, 12, -5, -6, 50, 3}, 4)
+	// arrAndStr.MinSubArrSum([]int{2, 3, 1, 2, 4, 3}, 7)
 	// Sliding Win - Dynamic
-	arrAndStr.MaxOnes([]int{1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0}, 2)
+	// arrAndStr.MaxOnes([]int{1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0}, 2)
 	// arrAndStr.MaxOnes([]int{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1}, 3)
+	arrAndStr.LongestWithSumLessThan([]int{1, 2, 3, 4, 5}, 8)
 
 }
